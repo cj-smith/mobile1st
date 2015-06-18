3d42b48b2c46a4fb0faf8ad39c8d5b708228c523
Caitlin <caitlin.smith007@gmail.com>
2015-06-18 16:17:33 -0700

    added functionality to move Main dive to the right when the button is pushed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@ $(document).ready(function(){
 	console.log('The js file is attached.');
 
 	$('input[value="moveright"]').click( function(){ 
-		$('.main').addClass( "displaced",200 );
+		$('.main').addClass( "displaced",1000 );
+	})
+
+	$('li').click( function(){ 
+		$('.main').removeClass( "displaced",500 );
 	})
 
 	console.log('After the function.');
